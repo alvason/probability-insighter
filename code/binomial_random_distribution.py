7690dce1c7a789274bd9aed9e437b17ec192aa01
A L <al@A-L.local>
2015-06-13 11:22:31 -0700

    poisson leveling

--- code/binomial_random_distribution.py
+++ code/binomial_random_distribution.py
@@ -28,7 +28,7 @@ AlvaFontSize = 23
 AlvaFigSize = (16, 7)
 numberingFig = 0
 
-'''Binomial process --- Binomial distribution'''
+# plotting
 figure_name = '-equation'
 file_suffix = '.png'
 save_figure = os.path.join(dir_path, file_name + figure_name + file_suffix)
@@ -37,13 +37,27 @@ numberingFig = numberingFig + 1
 plt.figure(numberingFig, figsize=(9, 6))
 plt.axis('off')
 plt.title(r'$ Binomial-distribution---probability-mass-function $',fontsize = AlvaFontSize)
-plt.text(0, 5.0/6, r'$ P_{b}(n|N) = \frac{N!}{n!(N - n)!} p^n (1 - p)^{N - n} $', fontsize = 1.2*AlvaFontSize)
+plt.text(0, 5.0/6, r'$ P(n|N) = \frac{N!}{n!(N - n)!} p^n (1 - p)^{N - n} $', fontsize = 1.2*AlvaFontSize)
 plt.text(0, 4.0/6, r'$ 1-- \ a \ set \ of \ N \ events $', fontsize = AlvaFontSize)
 plt.text(0, 3.0/6, r'$ 2-- \ either \ Success \ or \ Failure \ for \ each \ event $', fontsize = AlvaFontSize)
 plt.text(0, 2.0/6, r'$ 3-- \ the \ probability-p \ is \ the \ same \ for \ all \ events $', fontsize = AlvaFontSize)
 plt.text(0, 1.0/6, r'$ 4-- \ P(n|N) \ is \ the \ probability \ with \ n-success \ events \ in \ a \ total \ of \ N \ events $',
          fontsize = AlvaFontSize)
 plt.savefig(save_figure, dpi = 300)
+plt.show()
+
+# <codecell>
+
+numberingFig = numberingFig + 1
+plt.figure(numberingFig, figsize=(9, 6))
+plt.axis('off')
+plt.title(r'$ Binomial-distribution---cumulative-distribution-function $',fontsize = AlvaFontSize)
+plt.text(0, 5.0/6, r'$ P(n|N) = \sum_{m=0}^n \ \frac{N!}{m!(N - m)!} p^m (1 - p)^{N - m} $', fontsize = 1.2*AlvaFontSize)
+plt.text(0, 4.0/6, r'$ 1-- \ a \ set \ of \ N \ events $', fontsize = AlvaFontSize)
+plt.text(0, 3.0/6, r'$ 2-- \ either \ Success \ or \ Failure \ for \ each \ event $', fontsize = AlvaFontSize)
+plt.text(0, 2.0/6, r'$ 3-- \ the \ probability-p \ is \ the \ same \ for \ all \ events $', fontsize = AlvaFontSize)
+plt.text(0, 1.0/6, r'$ 4-- \ P(n|N) \ is \ the \ cumulative-probability \ with \ n-success \ events \ in \ a \ total \ of \ N \ events $',
+         fontsize = AlvaFontSize)
 plt.show()
 
 # <codecell>
@@ -61,8 +75,8 @@ def AlvaProduct(i):
 
 #testing
 i = 100
-print ('Alva = ', AlvaProduct(i))
-print ('NumP = ', np.prod(np.arange(1, i + 1), dtype=np.float64))
+print ('AlvaM =', AlvaProduct(i))
+print ('NumPy =', np.prod(np.arange(1, i + 1), dtype=np.float64))
 
 
 def AlvaBinomialD(i, N, p):
@@ -70,35 +84,112 @@ def AlvaBinomialD(i, N, p):
     B_distribution[:] = AlvaProduct(N)/(AlvaProduct(i[:])*AlvaProduct(N - i[:])) * p**i[:] * (1 - p)**(N - i[:])
     return (B_distribution)
 
+def AlvaBinomialC(m, N, p, binomialD):
+    B_C = 0.0*m
+    for j in range(np.size(m)):
+        for k in range(m[j]):
+            i = np.arange(k + 1)
+            B_distribution = binomialD(i, N, p)
+        B_C[j] = B_C[j] + B_distribution.sum()
+    return (B_C)
 
 total_event = int(30)
-i_event = np.arange(total_event + 1)
-totalPoint_Input = i_event.shape[0]
+i_event = np.arange(1, total_event + 1)
+totalPoint_Input = total_event
 probability_each = 0.5
-plt.figure(figsize = (12, 6))
-plt.plot(i_event, AlvaBinomialD(i_event, total_event, 0.3), marker ='o', color = 'red', label = 'p=3/10 Distribution')
-plt.plot(i_event, AlvaBinomialD(i_event, total_event, probability_each), marker ='o'
-         , color = 'green', label = 'p=5/10 Distribution')
-plt.plot(i_event, AlvaBinomialD(i_event, total_event, 0.7), marker ='o', color = 'blue', label = 'p=7/10 Distribution')
-plt.xlabel(r'$ output-level$', fontsize = AlvaFontSize)
-plt.ylabel(r'$ input/level $', fontsize = AlvaFontSize)
-plt.title(r'$ Binomial \ distribution $', fontsize = AlvaFontSize)
+
+binomial_D = AlvaBinomialD(i_event, total_event, probability_each)
+print ('total-probability = {:f}'.format(binomial_D.sum()))
+# plotting1
+figure = plt.figure(numberingFig, figsize = AlvaFigSize)
+plot1 = figure.add_subplot(1, 2, 1)
+plot1.plot(i_event, binomial_D, marker ='o', color = 'green')
 if totalPoint_Input < 100:
-    plt.axes().set_xticks(i_event, minor = True) 
-    plt.axes().set_yticks(AlvaBinomialD(i_event, total_event, probability_each), minor = True) 
-    plt.grid(True, which = 'minor')
+    plot1.set_xticks(i_event, minor = True) 
+    plot1.set_yticks(binomial_D, minor = True) 
+    plot1.grid(True, which = 'minor')
 else:
-    plt.grid(True, which = 'major')
-plt.legend(loc = (1, 0))
+    plot1.grid(True, which = 'major')
+plt.title(r'$ Binomial \ distribution-PMF \ (p={:}) $'.format(probability_each), fontsize = AlvaFontSize)
+plt.xlabel(r'$ n-event \ within \ total-event \ (N={:}) $'.format(total_event), fontsize = AlvaFontSize)
+plt.ylabel(r'$ P(n|N) $', fontsize = AlvaFontSize)
+plt.xticks(fontsize = AlvaFontSize*0.6)
+plt.yticks(fontsize = AlvaFontSize*0.6) 
+# plotting2
+i_event = np.arange(1, total_event + 1)
+binomial_C = AlvaBinomialC(i_event, total_event, probability_each, AlvaBinomialD)
+plot2 = figure.add_subplot(1, 2, 2)
+plot2.plot(i_event, binomial_C, marker ='o', color = 'red')
+if totalPoint_Input < 100:
+    plot2.set_xticks(i_event, minor = True) 
+    plot2.set_yticks(binomial_C, minor = True) 
+    plot2.grid(True, which = 'minor')
+else:
+    plot2.grid(True, which = 'major')
+plt.title(r'$ Binomial \ distribution-CDF \ (p={:}) $'.format(probability_each), fontsize = AlvaFontSize)
+plt.xlabel(r'$ n-event \ within \ total-event \ (N={:}) $'.format(total_event), fontsize = AlvaFontSize)
+plt.ylabel(r'$ P(n|N) $', fontsize = AlvaFontSize)
+plt.xticks(fontsize = AlvaFontSize*0.6)
+plt.yticks(fontsize = AlvaFontSize*0.6) 
+    
+figure.tight_layout()
 plt.show()
 
 # <codecell>
 
-'''Binomial randomness --- Binomial distribution (discrete)'''
+# plotting a list
+number_list = 9
+probability_each_list = np.arange(1, number_list + 1)*1.0/(number_list + 1)
+print ('probability_each_list =', probability_each_list)
+
+binomial_D_list = np.zeros([number_list, total_event])
+for i in range(number_list):
+    binomial_D_list[i] = AlvaBinomialD(i_event, total_event, probability_each_list[i])
+#    print ('total-probability = {:f}'.format(binomial_D_list[i].sum()))
+binomial_C_list = np.zeros([number_list, total_event])
+for i in range(number_list):
+    binomial_C_list[i] = AlvaBinomialC(i_event, total_event, probability_each_list[i], AlvaBinomialD)
+
+# plotting
+figure = plt.figure(numberingFig, figsize = AlvaFigSize)
+plot1 = figure.add_subplot(1, 2, 1)
+plot1.plot(i_event, binomial_D_list.T, marker ='o')
+if totalPoint_Input < 100:
+    plot1.set_xticks(i_event, minor = True) 
+    plot1.set_yticks(binomial_D_list[(number_list + 1)/2], minor = True) 
+    plot1.grid(True, which = 'minor')
+else:
+    plot1.grid(True, which = 'major')
+plt.title(r'$ Binomial \ distribution-PMF $', fontsize = AlvaFontSize)
+plt.xlabel(r'$ n-event \ within \ total-event \ (N={:}) $'.format(total_event), fontsize = AlvaFontSize)
+plt.ylabel(r'$ P(n|N) $', fontsize = AlvaFontSize)
+plt.xticks(fontsize = AlvaFontSize*0.6)
+plt.yticks(fontsize = AlvaFontSize*0.6) 
+
+plot2 = figure.add_subplot(1, 2, 2)
+plot2.plot(i_event, binomial_C_list.T, marker ='o')
+if totalPoint_Input < 100:
+    plot2.set_xticks(i_event, minor = True) 
+    plot2.set_yticks(binomial_C_list[(number_list + 1)/2], minor = True) 
+    plot2.grid(True, which = 'minor')
+else:
+    plot2.grid(True, which = 'major')
+plt.title(r'$ Binomial \ distribution-CDF $', fontsize = AlvaFontSize)
+plt.xlabel(r'$ n-event \ within \ total-event \ (N={:}) $'.format(total_event), fontsize = AlvaFontSize)
+plt.ylabel(r'$ P(n|N) $', fontsize = AlvaFontSize)
+plt.xticks(fontsize = AlvaFontSize*0.6)
+plt.yticks(fontsize = AlvaFontSize*0.6) 
+
+figure.tight_layout()
+plt.show()
+
+# <codecell>
+
+'''Binomial randomness --- Binomial distribution'''
 
 totalPoint_Input = int(100)
 gInput = np.arange(totalPoint_Input)
-output_level = 100
+output_level = totalPoint_Input
 probability_peak = 0.5
 randomSeed = np.random.binomial(output_level, probability_peak, totalPoint_Input)
 
@@ -107,13 +198,13 @@ for i in range(totalPoint_Input):
     sumI = sumI + randomSeed[i]
 meanI = sumI/(totalPoint_Input)
 
-totalLevel = int(totalPoint_Input/1)
+totalLevel = int(output_level)
 category = alva.AlvaLevel(randomSeed, totalLevel, False)
 gLevel = category[0]
 numberLevel = category[1]
 print category[2].shape
 
-binomial_D = 100*AlvaBinomialD(np.arange(totalLevel), totalLevel, probability_peak)
+binomial_D = totalPoint_Input*AlvaBinomialD(np.arange(totalLevel), totalLevel, probability_peak)
 
 # plotting
 figure_name = ''
@@ -131,10 +222,12 @@ if totalPoint_Input < 100:
     plot1.grid(True, which = 'minor')
 else:
     plot1.grid(True, which = 'major')
-plt.title(r'$ Binomial \ (total-input = %i,\ mean = %f) $'%(totalPoint_Input, meanI)
+plt.title(r'$ Binomial \ (total-event = %i,\ mean = %f) $'%(totalPoint_Input, meanI)
           , fontsize = AlvaFontSize)
-plt.xlabel(r'$ input $', fontsize = AlvaFontSize)
+plt.xlabel(r'$ event-input $', fontsize = AlvaFontSize)
 plt.ylabel(r'$ output $', fontsize = AlvaFontSize)
+plt.xticks(fontsize = AlvaFontSize*0.6)
+plt.yticks(fontsize = AlvaFontSize*0.6) 
 plt.legend(loc = (0, -0.2))
 
 plot2 = figure.add_subplot(1, 2, 2)
@@ -150,6 +243,8 @@ plt.title(r'$ Binomial \ distribution\ (data = %i,\ level = %i) $'%(totalPoint_I
           , fontsize = AlvaFontSize)
 plt.xlabel(r'$ input/level $', fontsize = AlvaFontSize)
 plt.ylabel(r'$ output-level $', fontsize = AlvaFontSize)
+plt.xticks(fontsize = AlvaFontSize*0.6)
+plt.yticks(fontsize = AlvaFontSize*0.6) 
 plt.legend(loc = (0, -0.2))
 
 figure.tight_layout()
--- code/poisson_random_distribution.py
+++ code/poisson_random_distribution.py
@@ -35,89 +35,110 @@ file_suffix = '.png'
 save_figure = os.path.join(dir_path, file_name + figure_name + file_suffix)
 
 numberingFig = numberingFig + 1
-plt.figure(numberingFig, figsize=(12, 3))
+plt.figure(numberingFig, figsize=(9, 6))
 plt.axis('off')
-plt.title(r'$ Poisson-distribution \ equation $',fontsize = AlvaFontSize)
-plt.text(0,2.0/3,r'$ P_{p}(n|N) = \frac{N!}{n!(N - n)!} p^n (1 - p)^{N - n} $', fontsize = 1.2*AlvaFontSize)
-plt.text(0,1.0/3,r'$ P_{m}(n) = \frac{e^{-m} m^n}{n!}, where \ the \ mean \ (average) \ m \equiv pN  $', fontsize = 1.2*AlvaFontSize)
+plt.title(r'$ Poisson-distribution---probability-mass-function $',fontsize = AlvaFontSize)
+plt.text(0, 3.0/4, r'$ P(m|n) = e^{-m} \frac{m^n}{n!} $', fontsize = 1.2*AlvaFontSize)
+plt.text(0, 2.0/4, r'$ 1-- \ m \ is \ the \ mean \ number \ of \ events \ per \ interval \ (time \ or \ space) $', fontsize = AlvaFontSize)
+plt.text(0, 1.0/4, r'$ 2-- \ P(m|n) \ is \ the \ probability \ with \ n-events \ per \ interval $',
+         fontsize = AlvaFontSize)
 plt.savefig(save_figure, dpi = 300)
 plt.show()
 
 # <codecell>
 
 def AlvaProduct(i):
-    product = 1
-    for j in range(1, int(i) + 1):        
-        product = product*j
-#       print product
-    return (product)
+    if type(i) != np.ndarray:
+        i = np.array([i])
+    A_product = 0.0*i + 1
+    for j in range(np.size(i)):
+        for k in range(1, i[j] + 1):        
+            A_product[j] = A_product[j]*k
+    if np.size(i) == 1:
+        # get rid of []
+        A_product = A_product[0]
+    return (A_product)
 
-def AlvaPoissonProcess(i, N):
-    if type(i) == np.ndarray:
-        total_Input = np.size(i)
-    p = np.zeros(total_Input)
-    process = np.zeros(total_Input)
-    for xn in range(total_Input):
-        p[xn] = 0.5
-        process[xn] = AlvaProduct(N)/(AlvaProduct(i[xn])*AlvaProduct(N - i[xn])
-                                      ) * p[xn]**i[xn] * (1 - p[xn])**(N - i[xn])
-#    print ('probability = ', p)
-    return (process)
+#testing
+#i = 100
+#print ('AlvaM =', AlvaProduct(i))
+#print ('NumPy =', np.prod(np.arange(1, i + 1), dtype=np.float64))
 
-def AlvaPoissonD(m, i):
-    if type(i) == np.ndarray:
-        total_Input = np.size(i)
-        distribution = np.zeros(total_Input)  
-        for xn in range(total_Input):
-            distribution[xn] = (m**i[xn])*np.exp(-m)/AlvaProduct(i[xn])
-    return (distribution)
+def AlvaPoissonD(i, meanP):
+    P_distribution = 0.0*i
+    P_distribution[:] = np.exp(-meanP) * meanP**i[:] / AlvaProduct(i[:])
+    return (P_distribution)
 
-N = float(100)
-b = 1
-mean = N*b/3
-rangeN = N*b
-print ('mean = ', mean)
-aaa =np.arange(1, N)*b
-print aaa
-plt.figure(figsize = (12, 4))
-plt.plot(aaa, AlvaPoissonProcess(aaa, N), marker ='^', color = 'blue', label = 'Process')
-plt.plot(aaa, AlvaPoissonD(mean, aaa), marker ='o', color = 'red', label = 'Distribution')
-#plt.plot(aaa, np.exp(-((aaa - mean)/rangeN)**2), marker ='+', color = 'red', label = 'Gaussian')
-plt.xlabel(r'$ Output-level$', fontsize = AlvaFontSize)
-plt.ylabel(r'$ Number/level $', fontsize = AlvaFontSize)
-plt.title(r'$ Poisson \ process $', fontsize = AlvaFontSize)
-plt.grid(True)
-plt.legend(loc = (1, 0))
+def AlvaPoissonC(m, meanP, poissonD):
+    B_C = 0.0*m
+    for j in range(np.size(m)):
+        for k in range(m[j]):
+            i = np.arange(k + 1)
+            B_distribution = poissonD(i, meanP)
+        B_C[j] = B_C[j] + B_distribution.sum()
+    return (B_C)
+
+total_event = int(30)
+i_event = np.arange(1, total_event + 1)
+totalPoint_Input = total_event
+meanP = total_event/3.0
+
+poisson_D = AlvaPoissonD(i_event, meanP)
+
+print ('total-probability = {:f}'.format(poisson_D.sum()))
+# plotting1
+figure = plt.figure(numberingFig, figsize = AlvaFigSize)
+plot1 = figure.add_subplot(1, 2, 1)
+plot1.plot(i_event, poisson_D, marker ='o', color = 'green')
+if totalPoint_Input < 100:
+    plot1.set_xticks(i_event, minor = True) 
+    plot1.set_yticks(poisson_D, minor = True) 
+    plot1.grid(True, which = 'minor')
+else:
+    plot1.grid(True, which = 'major')
+plt.title(r'$ Poisson \ distribution-PMF $', fontsize = AlvaFontSize)
+plt.xlabel(r'$ n-event \ with \ mean-event \ (m = {:}) $'.format(meanP), fontsize = AlvaFontSize)
+plt.ylabel(r'$ P(m|n) $', fontsize = AlvaFontSize)
+plt.xticks(fontsize = AlvaFontSize*0.6)
+plt.yticks(fontsize = AlvaFontSize*0.6) 
+
+# plotting2
+i_event = np.arange(1, total_event + 1)
+poisson_C = AlvaPoissonC(i_event, meanP, AlvaPoissonD)
+plot2 = figure.add_subplot(1, 2, 2)
+plot2.plot(i_event, poisson_C, marker ='o', color = 'red')
+if totalPoint_Input < 100:
+    plot2.set_xticks(i_event, minor = True) 
+    plot2.set_yticks(poisson_C, minor = True) 
+    plot2.grid(True, which = 'minor')
+else:
+    plot2.grid(True, which = 'major')
+plt.title(r'$ Poisson \ distribution-CDF $', fontsize = AlvaFontSize)
+plt.xlabel(r'$ n-event \ with \ mean-event \ (m = {:}) $'.format(meanP), fontsize = AlvaFontSize)
+plt.ylabel(r'$ P(m|n) $', fontsize = AlvaFontSize)
+plt.xticks(fontsize = AlvaFontSize*0.6)
+plt.yticks(fontsize = AlvaFontSize*0.6) 
+
+figure.tight_layout()
 plt.show()
 
 # <codecell>
 
 '''Poisson randomness --- Poisson distribution'''
-totalPoint_Input = int(200 + 1)
-gInput = np.arange(totalPoint_Input)
-meanP = totalPoint_Input/20
-randomSeed_normal = np.random.standard_normal(totalPoint_Input) + meanP
-randomSeed = np.random.poisson(meanP, totalPoint_Input)
+total_event = int(300)
+gInput = np.arange(total_event)
+meanP = 10.0
+randomSeed = np.random.poisson(meanP, total_event)
 
-totalLevel = int(totalPoint_Input/1)
+totalLevel = int(total_event/10)
 category = alva.AlvaLevel(randomSeed, totalLevel, False)
 gLevel = category[0]
+gLevel_int = gLevel.astype(int)
 numberLevel = category[1]
-print category[2].shape
+print ('level =', gLevel)
+print ('level_int =', gLevel_int)
 
-# calculating the mean
-sumP = 0
-for i in range(totalPoint_Input):
-    sumP = sumP + randomSeed[i]
-current_mean = sumP/(totalPoint_Input)
-print ('current mean', current_mean)
-
-totalLevel = int(totalPoint_Input/1)
-category = alva.AlvaLevel(randomSeed, totalLevel, False)
-gLevel = category[0]
-numberLevel = category[1]
-print category[2].shape
-
+poisson_D = total_event*AlvaPoissonD(gLevel_int, meanP)
 # plotting
 figure_name = ''
 file_suffix = '.png'
@@ -128,65 +149,38 @@ figure = plt.figure(numberingFig, figsize = AlvaFigSize)
 plot1 = figure.add_subplot(1, 2, 1)
 plot1.plot(gInput, randomSeed, color = 'gray', marker = 'o', label = 'data')
 plot1.plot(gInput, alva.AlvaMinMax(randomSeed), color = 'red', marker = 'o', label = 'minMaxListing')
-plot1.plot(gInput, alva.AlvaMinMax(randomSeed_normal), label = 'minMax_normal')
-if totalPoint_Input < 100:
+if total_event < 100:
     plot1.set_xticks(gInput, minor = True) 
     plot1.set_yticks(randomSeed, minor = True)
     plot1.grid(True, which = 'minor')
 else:
     plot1.grid(True, which = 'major')
-plt.title(r'$ Poisson\ (total-input = %i,\ mean = %f) $'%(totalPoint_Input, meanP)
-          , fontsize = AlvaFontSize)
-plt.xlabel(r'$ input $', fontsize = AlvaFontSize)
+plt.title(r'$ Poisson \ randomness \ (mean = {:}) $'.format(meanP), fontsize = AlvaFontSize)
+plt.xlabel(r'$ event-input $', fontsize = AlvaFontSize)
 plt.ylabel(r'$ output $', fontsize = AlvaFontSize)
 plt.legend(loc = (0, -0.2))
+plt.xticks(fontsize = AlvaFontSize*0.6)
+plt.yticks(fontsize = AlvaFontSize*0.6) 
 
 plot2 = figure.add_subplot(1, 2, 2)
+plot2.plot(poisson_D, gLevel_int, color = 'blue', marker = 'o', label = 'Poisson Distribution') 
 plot2.plot(numberLevel, gLevel, color = 'red', marker = 'o', label = 'category')
-plot2.plot(AlvaPoissonD(meanP, gLevel), gLevel, color = 'blue', marker = 'o', label = 'Poisson Distribution') 
-plot2.plot(AlvaPoissonProcess(gLevel, 18), gLevel, color = 'green', marker = 'o', label = 'Poisson Process') 
-if totalPoint_Input < 100:
+if totalLevel < 100:
     plot2.set_xticks(numberLevel, minor = True) 
     plot2.set_yticks(gLevel, minor = True)
     plot2.grid(True, which = 'minor')
 else:
     plot2.grid(True, which = 'major')
-plt.title(r'$ Poisson \ distribution\ (data = %i,\ level = %i) $'%(totalPoint_Input, totalLevel)
+plt.title(r'$ Poisson \ distribution \ (event = {ev:},\ level = {le:}) $'.format(ev = total_event, le = totalLevel)
           , fontsize = AlvaFontSize)
-plt.xlabel(r'$ Number/level $', fontsize = AlvaFontSize)
-plt.ylabel(r'$ Output-level $', fontsize = AlvaFontSize)
+plt.xlabel(r'$ event/level $', fontsize = AlvaFontSize)
+plt.ylabel(r'$ magnitude \ of \ level $', fontsize = AlvaFontSize)
 plt.legend(loc = (0, -0.2))
+plt.xticks(fontsize = AlvaFontSize*0.6)
+plt.yticks(fontsize = AlvaFontSize*0.6) 
 
 figure.tight_layout()
 plt.savefig(save_figure, dpi = 300)
-plt.show()
-
-# <codecell>
-
-i = 100
-print ('Alva = ', AlvaProduct(i))
-print ('NumP = ', np.prod(np.arange(1, i + 1), dtype=np.float64))
-
-# <codecell>
-
-''' Gaussian Distribution '''
-unitD = 1
-rangeD = float(100)*unitD
-meanD = rangeD/2
-varianceD = rangeD
-print ('mean = ', mean)
-aaa =np.arange(1, rangeD)*b
-print aaa
-plt.figure(figsize = (12, 4))
-#plt.plot(aaa, AlvaPoissonProcess(aaa, N), marker ='^', color = 'blue', label = 'Process')
-#plt.plot(aaa, AlvaPoissonD(mean, aaa), marker ='o', color = 'red', label = 'Distribution')
-plt.plot(aaa, np.exp(-((aaa - meanD)/varianceD)**2), marker ='+', color = 'red', label = 'Gaussian')
-plt.plot(aaa, np.exp(-((aaa - meanD)**2/varianceD)), marker ='+', color = 'blue', label = 'Gaussian')
-plt.xlabel(r'$ Output-level$', fontsize = AlvaFontSize)
-plt.ylabel(r'$ Number/level $', fontsize = AlvaFontSize)
-plt.title(r'$ Poisson \ process $', fontsize = AlvaFontSize)
-plt.grid(True)
-plt.legend(loc = (1, 0))
 plt.show()
 
 # <codecell>
